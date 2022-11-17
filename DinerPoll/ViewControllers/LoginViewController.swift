f9a05e11ebf863f050f401ad0e92a8be3baf9df2
id2k1149 <id2k1149@gmail.com>
2022-11-17 12:49:34 -0500

    add login button

--- DinerPoll/ViewControllers/LoginViewController.swift
+++ DinerPoll/ViewControllers/LoginViewController.swift
@@ -7,11 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
+    @IBOutlet var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loginButton.layer.cornerRadius = 10
     }
 
 
