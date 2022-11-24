7e492921d14df4f47db0b9baa517a6a52ce415c2
id2k1149 <id2k1149@gmail.com>
2022-11-23 20:10:21 -0500

    edit override func tableView

--- DinerPoll/ViewControllers/DinersTableViewController.swift
+++ DinerPoll/ViewControllers/DinersTableViewController.swift
@@ -31,14 +31,9 @@ class DinersTableViewController: UITableViewController {
         
         var content = cell.defaultContentConfiguration()
         
-        for i in 1...diners[indexPath.section].menu.count {
-            
-//            if indexPath.item == i - 1 {
-//                content.text = "\(i). \(menu[indexPath.item].0) for \(menu[indexPath.item].1)"
-//            }
-            
-            if indexPath.item == i - 1 {
-                content.text = "\(i). "
+        for (index, menu) in diners[indexPath.section].menu.enumerated() {
+            if indexPath.item == index {
+                content.text = "\(index + 1). \(menu.key) for \(menu.value)"
             }
         }
 
